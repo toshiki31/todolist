241cd5c92c897ec8554894e3b5c68ecc2f99f60f
toshiki31 <toshiki.m.020131@outlook.jp>
2023-02-13 04:23:30 +0900

    [add] Lineログイン

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,11 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
+            $table->string('provider')->nullable();
+            $table->string('line_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
